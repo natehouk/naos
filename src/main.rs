6449ee1abdafd738153a161b842234427a1b0105
Nate Houk <nate.houk@gmail.com>
2022-02-26 10:52:18 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
